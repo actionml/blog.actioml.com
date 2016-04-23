93ba5e438dc897aa9712e05c71e0996daa4f44ac
pote <pote@tardis.com.uy>
2016-04-23 08:26:36 +0200

    Adds blog index post metadata

--- metadata.js
+++ metadata.js
@@ -1,4 +1,11 @@
 PostList = [
-  {title: 'A Demo is Worth a Thousand Words', template: "a_demo"},
+  {
+    title: "A Demo is Worth a Thousand Words",
+    template: "a_demo",
+    excerpt: "Last year using an older version of the Universal Recommender I built a little demo app. It runs in my closet on an old desktop over Comcast so don’t...",
+    author: "Pat Ferrel",
+    date: "Apr 1, 2016",
+    thumb: "/images/Guide-front-1024x7211.png"
+  },
   {title: 'Second blog post', template: "second_blog_post"},
 ];
