8f3f253c7f259b4ef288faeb82c12307660e3476
pote <pote@tardis.com.uy>
2016-05-08 21:26:26 +0200

    Test categories

--- metadata.js
+++ metadata.js
@@ -3,17 +3,18 @@ PostList = [
     title: "A Markdown Test",
     template: "posts_md_test",
     excerpt: "posts_md_test_excerpt",
+    categories: ['News', 'Tips']
 },
 {
     title: "A Demo is Worth a Thousand Words",
     template: "a_demo",
     excerpt: "a_demo_excerpt",
-    categories: ["use case", "recommender"],
+    categories: ["Use Case", "Recommender"],
 },
 {
     title: 'Second blog post',
     template: "second_blog_post",
     excerpt: "second_blog_post_excerpt",
-    categories: ["test", "templates"],
+    categories: ['Recommender', 'Writing', 'Tips'],
 },
 ];
