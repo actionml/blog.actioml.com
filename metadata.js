d0a8f4e281c5f0e481210f5780f2a4eb2bf57055
pferrel <pat@occamsmachete.com>
2016-10-05 09:29:15 -0700

    cco post

--- metadata.js
+++ metadata.js
@@ -3,23 +3,23 @@ PostList = [
         title: "Correlated Cross-Occurrence (CCO): Not All Events are Created Equal",
         template: "cco",
         excerpt: "cco_excerpt",
-        categories: ["recommender", "datascience", "anecdote"],
+        categories: ["recommender", "datascience", "anecdote"]
     },{
         title: "Personalized Search",
         template: "personalized_search",
         excerpt: "personalized_search_excerpt",
-        categories: ["use case", "recommender"],
+        categories: ["use case", "recommender"]
     },
     {
         title: "A Demo is Worth a Thousand Words",
         template: "a_demo",
         excerpt: "a_demo_excerpt",
-        categories: ["use case", "recommender"],
+        categories: ["use case", "recommender"]
     },
     {
         title: "The Universal Recommender",
         template: "ur_algo_slides",
         excerpt: "ur_algo_slides_excerpt",
-        categories: ["use case", "recommender"],
+        categories: ["use case", "recommender"]
     }
 ];
